8279af0cb9f8f1f8e0fb3d792f4d0c44601316e9
Bronson Brown <Brsn90@icloud.com>
2019-10-31 15:58:39 -0600

    Add files via upload

---
+++ linkedlist.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+//const n1 = new Node(100);
+//console.log(n1);
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+}
+ insertFirst(data) {
+  this.head = new Node(data, this.head);
+}
+ printListData() {
+     let current = this.head;
+
+     while(current) {
+         console.log(current.data);
+         current = current.next;
+     }
+ }
+}
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(200);
+console.log(ll);
